fe25565f2bf08b7c46c5e5cb1fc3a82fe037832f
renatocuellar <matealva@gmail.com>
2024-11-14 21:44:33 -0500

    Aprendimos a usar lambdas

---
+++ 03_lambdas.py
@@ -0,0 +1,7 @@
+# Lambdas
+
+sum_two_value = lambda first_value, second_value: first_value + second_value
+print(sum_two_value(5, 8))
+
+multiply_value = lambda first_value, second_value: first_value * second_value
+print(multiply_value(35, 4))
