860957f7a5be7367e49b4c92fc21d98d3e3154b7
renatocuellar <matealva@gmail.com>
2024-11-19 15:50:21 -0500

    Trabajamos creando archivos con diferentes extensiones

--- 06_file_handing.py
+++ 06_file_handing.py
@@ -1,5 +1,7 @@
 ### File Handing ###
 
+import os
+
 # .txt file
 
 txt_file = open("my_file.txt", "r+") # Leer y escribir
@@ -9,3 +11,36 @@ txt_file = open("my_file.txt", "r+") # Leer y escribir
 
 for line in txt_file.readlines():
     print(line)
+
+txt_file.close()
+
+#txt_file.write("\n Aunque también me gusta JavaScript")
+
+
+import json
+
+json_file = open("my_file.json", "r+")
+
+json_test = {"Name":"Renato",
+             "Surname": "Cuellar",
+             "Age": 35, 
+             "Languages": ["Python", "JavaScript", "Kotlin", "Java", "Swift", "TypeScript", "Php", "Ruby"],
+             "Website": "https://renatocuellar.com"
+             }
+
+json.dump(json_test, json_file, indent = 4)
+print(json_file.read())
+
+# .cvs file
+
+import csv
+
+csv_file = open("my_csv.csv", "w+")
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerows(["name", "surname", "Age", "Language", "Website"])
+csv_writer.writerows(["Renato", "Cuéllar", 35, "Python", "https://renatocuellar.com"])
+
+# .xml file
+
+import xml
